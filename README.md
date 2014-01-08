a47aea0decf2353d4346dfe2209bc8c76bffede6
Simon Henrotte <henrotte.simon@gmail.com>
2014-01-08 16:09:46 +0100

    Ng-Fluidvids v0.1.0

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-ng-fluidvids
-============
+# Ng-Fluidvids
 
-Angular directives to have fluid embedded video
+Ng-Fluidvids allow you to embed a fluid videos from any provider has Youtube, Daylimotion, Vimeo, ... .
+
+## Goals
+
+Give an easy solution to embed fluid video inside a website from any providers on the web.
+
+## Usage
+
+1. Add the CSS code for fluidvids from CSS file (assets/css/screen.css).
+2. Get the ng-fluidvids directive inside your own app (assets/js/scripts/ng-fluidvids.js).
+3. Defined mark-up for fluidvids
+
+## Implements
+
+### Mark-up
+
+	<div fluidvids video="url-here" width="width-here" height="height-here"></div>
+	<fluidvids video="url-here" width="width-here" height="height-here"></fluidvids>
+
+### Example
+
+	<div fluidvids video="http://www.youtube.com/embed/dGQPkOZMBFs" height="281" width="500"></div>
+
+Custom tag is not supported everywhere so, check the compatibility of your browser.
---
+++ assets/js/scripts/ng-fluidvids.js
@@ -0,0 +1,41 @@
+var ngFluidVids = angular.module('myApp', []);
+
+ngFluidVids.config(function($sceDelegateProvider) {
+    // The whitelist set specific ressources origin to be allowed
+    $sceDelegateProvider.resourceUrlWhitelist([
+        // Allow same origin resource loads.
+        'self',
+
+        // Allow from specific server. 
+        // * = one value 
+        // ** = any number of value
+        // 'http://srv*.assets.example.com/**',
+        // Allow from everywhere
+        '**'
+    ]);
+
+    // The blacklist set specific ressources origin to be denied
+    $sceDelegateProvider.resourceUrlBlacklist([]);
+});
+
+ngFluidVids.directive('fluidvids', function($sce) {
+    return {
+        // ‘A’ – Attribute (You want to use your directive as <div rating>)
+        // ‘E’ – Element (Use it as <rating>) Olders browser doesn't support custom tag elements
+        // ‘C’ – Class. (Use it like <div class=”rating”>)
+        // ‘M’ – Comment (Use it like <!– directive: rating –>
+        restrict: 'AE',
+        replace: true,
+        transclude: true,
+        scope: {
+            video: '@'
+        },
+        template: '<div class="fluidvids">' +
+            '<iframe ng-src="{{ video }}" allowfullscreen frameborder="0"></iframe>' +
+            '</div>',
+        link: function($scope, element, attrs) {
+            var ratio = (attrs.height / attrs.width) * 100;
+            element[0].style.paddingTop = ratio + '%';
+        }
+    }
+});
